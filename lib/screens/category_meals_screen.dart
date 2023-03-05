9afb96a17752ceab8ec17134ab1dbd631bd8b7b8
Alikhan Kaliyev <alikhanfrontend22@gmail.com>
2023-03-05 14:55:09 +0600

    added everything

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+
+import '../models/meal.dart';
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const routeName = '/category-meals';
+
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen(this.availableMeals);
+  @override
+  State<CategoryMealsScreen> createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  late String categoryTitle;
+  late List<Meal> displayedMeals;
+
+  var _loadedInitData = false;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+      final routeArgs =
+          ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+      final String categoryId = routeArgs['id'] as String;
+      categoryTitle = routeArgs['title'] as String;
+      displayedMeals = widget.availableMeals.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+       _loadedInitData = false;
+    }
+    super.didChangeDependencies();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          categoryTitle,
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            id: displayedMeals[index].id,
+            title: displayedMeals[index].title,
+            imageUrl: displayedMeals[index].imageUrl,
+            duration: displayedMeals[index].duration,
+            complexity: displayedMeals[index].complexity,
+            affordability: displayedMeals[index].affordability,
+          );
+        },
+        itemCount: displayedMeals.length,
+      ),
+    );
+  }
+}
